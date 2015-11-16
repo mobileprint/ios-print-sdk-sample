847fcd8748e403144dd0fec2f695f8125ac12680
James <trask@hp.com>
2015-11-16 09:42:02 -0800

    Initial Commit

---
+++ PrintSDKSample/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  PrintSDKSample
+//
+//  Created by James Trask on 11/16/15.
+//  Copyright © 2015 HP. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ PrintSDKSample/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  PrintSDKSample
+//
+//  Created by James Trask on 11/16/15.
+//  Copyright © 2015 HP. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
